3a38ce734f5afaa9f0390ab545e282101aac9e8c
sharklemon <anastasia.l.mark@gmail.com>
2017-08-03 21:16:41 -0400

    some css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,31 +6,44 @@ var questions = [
 		"answer2": "20 items per second",
 		"answer3": "35 items per second",
 		"answer4": "100 items per secon",
-		"correct": "answer3"
+		"correct": "answer3",
+		"img": "https://www.propublica.org/images/ngen/gypsy_big_image/20160920-amazon-630x420.gif"
 	},
 	{
-		"question": "How many unique visitors does Amazon.com get?",
-		"answer1": "100 visitors per minute",
-		"answer2": "2000 visitors per minute",
-		"answer3": "3700 visitors per minute",
-		"answer4": "4300 visitors per minute",
-		"correct": "answer4"
+		"question": "What was the first science fiction book?",
+		"answer1": "Frankenstein",
+		"answer2": "Princess of Mars",
+		"answer3": "Tale of Two Cities",
+		"answer4": "Lord of the Rings",
+		"correct": "answer1",
+		"img": "https://imgs-tuts-dragoart-386112.c.cdn77.org/how-to-draw-cute-frakenstein_1_000000021174_5.png"
 	},
 	{
-		"question": "What's the longest running T.V. show in the US? (prime-time, scripted)",
-		"answer1": "The Simpsons",
-		"answer2": "Law & Order",
-		"answer3": "American Idol",
-		"answer4": "Days of our Lives",
-		"correct": "answer1"
+		"question": "What does the 'four score' in 'four score and seven years ago' mean?",
+		"answer1": "Many years",
+		"answer2": "80 [years]",
+		"answer3": "A century",
+		"answer4": "40 [years]",
+		"correct": "answer2",
+		"img": "http://cdn.history.com/sites/2/2013/12/gettysburg-address-memorial2-AB.jpeg"
+	},
+	{
+		"question": "How old was Joan of Arc when she died?",
+		"answer1": "15",
+		"answer2": "17",
+		"answer3": "19",
+		"answer4": "21",
+		"correct": "answer3",
+		"img": "http://static.panoramio.com/photos/large/79687436.jpg"
 	}
-];
+];	
 
 var itertimer; // Variable itertimer will hold the setInterval when we start the slideshow
 
 
 var questionindex = 0;
 var answerindex;
+var remainder = questions.length;
 
 var thisanswer;
 var thisquestion;
@@ -42,8 +55,13 @@ var timer = 5;
 
 
 function NoResponse(){
-	$(".YOUWERE").text("You ran out of time.");
-	wrong++
+	console.log("in no response");
+	var OUT = $("h1");
+	OUT.text("You ran out of time.");
+	$(".YOUWERE").append(OUT);
+	wrong++;
+	$("#Wrong").html(wrong);
+	console.log("wrong is " +wrong)
 	resultz();  
 }
 
@@ -70,6 +88,18 @@ $(".ansr").on("click", function(){
 		console.log("in the if");
 	}
 	Response();
+});
+
+$("#nexter").on("click", function(){
+ 	questionindex++;
+ 	console.log("in nexterbuttonfunction")
+ 	if (questionindex >=questions.length){
+  		console.log("You're done!");
+  	}
+  	else{
+  		console.log("nextQ should go")
+		changeDisplay(questionindex);
+		}
 });
 
 
@@ -101,18 +131,23 @@ function changeDisplay(z) {
 function Response(){
 	console.log("in Response");
 	//console.log ("user answer is: " + UserAnswer + " and real answer is " +answerindex)
+	$("jumbotron").removeClass('hidden');
 
   	if (UserAnswer == answerindex){
 	  	console.log("in response = correct");
 		correct++
-		$(".YOUWERE").text("You were right!");
+		var YES = $("h1");
+		YES.text("You were right!");
+		$(".YOUWERE").append(YES);
 	  	$("#Correct").html(correct);
 		resultz();	
 	}
   	else{
 		console.log("in response = wrong");
 		wrong++
-		$(".YOUWERE").text("Nope.");
+		var NOPE = $("h1");
+		NOPE.text("Nope.");
+		$(".YOUWERE").append(NOPE);
 		$("#Wrong").html(wrong);
 		resultz();	
 	}	  	
@@ -126,18 +161,12 @@ function resultz(){
 	//display correct answer & next button with functionality
 	$("#questionR").html(thisquestion);
 	$("#correctR").html("The answer was "+ thisanswer);
+	remainder--;
+	console.log("remainder " + remainder);
+	$("#Questions_Remaining").html(remainder);
+	$("#timer").html("0 seconds left");	
 	$("#nexter").show();
-	$("#nexter").on("click", function(){
-	 	questionindex++;
-	 	console.log("in nexterbuttonfunction")
-	 	if (questionindex >=questions.length){
-	  		console.log("You're done!");
-	  	}
-	  	else{
-	  		console.log("nextQ should go")
-			changeDisplay(questionindex);
-  		}
-	});
+	$("jumbotron").removeClass('hidden');
 };	
 
 
@@ -147,7 +176,7 @@ function countDown(){
 	if(timer > 0){
 		setTimeout(countDown,1000);
 		console.log(timer);
-		$("#timer").html(timer);
+		$("#timer").html(timer+ " seconds left");
 	}
 	timer--;
 }
